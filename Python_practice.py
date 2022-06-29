2d4654c1322d292f6b8d685c14d9943394f627fd
Angelica Rosario <angelicarosario86@gmail.com>
2022-06-28 21:36:18 -0700

    Add updated challenge

---
+++ Python_practice.py
@@ -0,0 +1,122 @@
+print("Hello World")
+
+counties=["Arapahoe", "Denver", "Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties")
+
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+
+if "Arapahoe" in counties and "El Paso" not in counties:
+   print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+count = 7
+while count < 1:
+    print("Hello World")
+
+for county in counties:
+    print(county)
+
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+
+for num in range(5):
+    print(num)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+counties_dict={"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict.keys():
+    print(county)
+for county in counties_dict.keys():
+    print("Arapahoe")
+    print("Denver")
+    print("Jefferson")
+
+for voters in counties_dict.values():
+    print(voters)
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+for key, value in counties_dict.items():
+    print(key, value)
+
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+for county, voters in counties_dict.items():
+    print(county,"county has", voters, "registered voters.")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+
+my_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
+
+
+
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
